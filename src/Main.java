0c4927316858a312e1eecda0f3b7287199dc58bc
MarlenZeyn <zmarlen45@gmail.com>
2022-11-23 07:39:01 +0600

    hw операции

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("text");
+    int a = 10, b = 5, x=1;
+        System.out.println(a*b);
+        System.out.println(20/2.5);
+    int Length = 15, width = 9, s, p;
+    s = Length*width;
+    p = 2*(Length+width);
+        System.out.println("s="+s+" "+"p="+p);
+        ++x;
+        System.out.println(x);
+        x++;
+        System.out.println(x);
+        System.out.println(x*=5);
+        System.out.println(x/=5);
+        System.out.println(25==43);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        int fah=320,cel;
+        cel = ((fah-32)*5)/9;
+        System.out.println(cel);
+        int t = 10,t1;
+        t1 = (((t + 451)%3)+5)*5;
+        System.out.println(t1);
+        int a1 = 65, b1 = 80;
+        System.out.println(a1<50);
+        System.out.println(a1<b1);
+
+
+
+
 
 
 
